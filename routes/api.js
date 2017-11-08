2a9383b04d0990cf1e4e2e0148c2e64e99b61117
Axel Dolce <axel.dolce@gmail.com>
2017-11-08 18:19:13 -0300

    refactor

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
+var LocationService = require('../services/location')
 var db = require('../db')
 var asd = (async () => {
     //return
@@ -27,12 +28,47 @@ router.get('/profiles', (req, res, next) => {
     })
 })
 
+router.use('/profiles/stats/byLocation', (req, res, next) => {
+    var sql = 'SELECT p.LocationId as id, l.name, COUNT(1) as profiles FROM Profiles p JOIN Locations l ON p.LocationId=l.id GROUP BY p.LocationId ORDER BY profiles DESC, l.name'
+    db.query(sql, { type: db.Sequelize.QueryTypes.SELECT }).then(rows => {
+        res.json({
+            meta: sql,
+            data: rows
+        })
+    }).catch(next)
+})
+
 router.get('/locations', (req, res, next) => {
     var Location = db.model('Location')
     Location.all().then(locations => {
         var data = locations.map(i => i.get())
         res.json({ data })
     })
+})
+
+router.get('/locations/:id', async (req, res, next) => {
+    var Location = db.model('Location')
+    try {
+        var location = await Location.findById(req.params.id)
+        var children = await LocationService.getChildLocations(req.params.id)
+        var locations = await Location.all({
+            where: {
+                id: {
+                    [db.Op.in]: children
+                }
+            }
+        })
+        res.json({
+            data: {
+                name: location.get('name'),
+                location,
+                children,
+                locations
+            }
+        })
+    } catch (err) {
+        return next(err)
+    }
 })
 
 router.get('/queries', async (req, res, next) => {
